7ecab21bac404d1e01bb3cc034a30d1e1de3585a
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2010-10-11 12:29:59 +0000

    Make groktoolkit into a "python package" to play nicely with zest.releaser. Automate the upload of the versions.cfg file

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+version = '1.2dev'
+
+setup(
+    name='groktoolkit',
+    version=version,
+    description='Grok: Now even cavemen can use Zope 3!',
+    author='Grok Team',
+    author_email='grok-dev@zope.org',
+    url='http://grok.zope.org',
+    license='ZPL',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zc.buildout',
+        'zest.releaser',
+        ],
+    entry_points = {
+        'zest.releaser.releaser.after': [
+            'upload_ztk_versions=groktoolkit:upload_entrypoint',
+            ],
+        }
+    )
