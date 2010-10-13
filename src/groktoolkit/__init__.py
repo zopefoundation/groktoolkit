f9f713644b1ecf50ce11beb17d674e648d986520
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2010-10-13 10:22:41 +0000

    actually add the code for the release entry point

---
+++ src/groktoolkit/__init__.py
@@ -0,0 +1,29 @@
+import sys
+import re
+import os
+import commands
+
+HOST = 'grok.zope.org'
+RELEASEINFOPATH = '/var/www/html/grok/releaseinfo'
+
+def _upload_gtk_versions(packageroot, version):
+    # ``scp`` the file to the given destination.
+    versions_filename = os.path.join(packageroot, 'grok.cfg')
+    cmd = 'scp %s %s:%s/%s/versions.cfg' % (
+        versions_filename, HOST, RELEASEINFOPATH, version)
+    print cmd + '\n'
+    print commands.getoutput(cmd)
+
+def upload_entrypoint(data):
+    if data['name'] != 'groktoolkit':
+        # We're dealing with another package that somehow depends on
+        # groktoolkit. Skip the step in that case.
+        return
+    packageroot = data['workingdir']
+    version = data['version']
+    _upload_gtk_versions(packageroot, version)
+
+def upload_gtk_versions():
+    packageroot = os.getcwd() # Ugh.
+    version = sys.argv[1] # Ugh.
+    _upload_gtk_versions(packageroot, version)
