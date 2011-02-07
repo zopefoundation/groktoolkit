87e0c8637636af58593c14f601b4583451f86cc8
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2011-02-07 14:48:10 +0000

    releases will be a single document, remove the directory

--- doc/scripts/update-releaseinfo.py
+++ doc/scripts/update-releaseinfo.py
@@ -3,26 +3,26 @@
 # and heavily modified.
 
 import ConfigParser
-import StringIO
 import os
 import os.path
+import py.path
 import socket
+import StringIO
+import sys
 import urllib2
 import xml.etree.ElementTree
-import py.path
 
 socket.setdefaulttimeout(10)
 
 TABLE_HEADER = """\
 .. list-table::
     :class: packagelist
-    :widths: 25 10 40 25
+    :widths: 25 10 65
     :header-rows: 1
 
     * - Name
       - Version
-      - Description
-      - Links\
+      - Description\
 """
 
 PACKAGE_LINE_BASE = """
@@ -31,14 +31,7 @@ PACKAGE_LINE_BASE = """
       - %(description)s\
 """
 
-DEPENDENCY_PACKAGE_LINE = PACKAGE_LINE_BASE + """
-      - \
-"""
-
-PACKAGE_LINE = PACKAGE_LINE_BASE + """
-      - `Bugs <http://bugs.launchpad.net/%(name)s>`_ |
-        `Subversion <http://svn.zope.org/%(name)s>`_ \
-"""
+PACKAGE_LINE = PACKAGE_LINE_BASE
 
 GENERATED_WARNING = """\
 .. This file is generated. Please do not edit manually or check in.
@@ -46,6 +39,7 @@ GENERATED_WARNING = """\
 
 DOAP_NS = 'http://usefulinc.com/ns/doap#'
 GROKTOOLKIT_ROOT = py.path.svnurl('http://svn.zope.org/repos/main/groktoolkit')
+TAGS_URL = GROKTOOLKIT_ROOT/'tags'
 
 def package_list(
         packages, config, out, line=PACKAGE_LINE):
@@ -58,9 +52,10 @@ def package_list(
         doap_xml = StringIO.StringIO(doap_xml.replace('\f', ''))
         doap = xml.etree.ElementTree.ElementTree()
         doap.parse(doap_xml)
-        description = doap.find('//{%s}shortdesc' % DOAP_NS).text
+        description = ' '.join(
+            doap.find('//{%s}shortdesc' % DOAP_NS).text.splitlines())
         homepage = 'http://pypi.python.org/pypi/%s/%s' % (package, version)
-        print >>output, line % dict(
+        print >>out, line % dict(
             name=package, homepage=homepage,
             description=description, version=version)
     print >>out
@@ -73,50 +68,62 @@ def packages(config, key):
     result = filter(None, map(str.strip, result))
     return result
 
-releases = []
-tags_url = GROKTOOLKIT_ROOT/'tags'
+def get_releases():
+    tags = TAGS_URL.listdir()
+    tags.reverse()
+    releases = []
+    for tag in tags:
+        releases.append((tag.basename, tag))
+        break
+    return releases
 
-for tag_url in tags_url.listdir().reverse():
-    releases.append((tag_url.basename, tag_url))
+def write_package_lists(releases, path):
+    for release, location in releases:
+        print 'Writing package list for "%s"' % release
 
-for release, location in releases:
-    print 'Writing package list for "%s"' % release
+        config = ConfigParser.RawConfigParser()
+        config.optionxform = str
+        fp = StringIO.StringIO((location/'grok.cfg').read())
+        config.readfp(fp)
 
-    config = ConfigParser.RawConfigParser()
-    config.optionxform = str
-    fp = StringIO.StringIO((location/'grok.cfg').read())
-    config.readfp(fp)
+        output = open(os.path.join(path, 'packages-%s.rst' % release), 'w')
 
-    output = open(
-        os.path.join('doc', 'releases', 'packages-%s.rst' % release), 'w')
+        print >>output, GENERATED_WARNING
 
+        heading = 'Grok %s packages' % release
+        print >>output, '=' * len(heading)
+        print >>output, heading
+        print >>output, '=' * len(heading)
+
+        ztk_version = '1.1'
+        print >>output, 'Zope Toolkit %s' % ztk_version
+        print >>output, '------------------------------'
+        print >>output, '`Overview of ZTK-%s <http://docs.zope.org/zopetoolkit/releases/overview-%s.html>`' % (ztk_version, ztk_version)
+
+        print >>output, 'Packages'
+        print >>output, '--------'
+        included = packages(config, 'included')
+        package_list(included, config, output)
+
+        deprecating = packages(config, 'deprecating')
+        if deprecating:
+            print >>output, 'Deprecating'
+            print >>output, '-----------'
+            package_list(deprecating, versions, output)
+
+        print >>output, 'Other dependencies'
+        print >>output, '------------------'
+        all = config.options('versions')
+        dependencies = (set(all) - set(included)) - set(deprecating)
+        package_list(dependencies, config, output)
+        output.close()
+
+def write_overview(releases, path):
+    print "Writing overview"
+
+    output = open(os.path.join(path, 'index.rst'), 'w')
     print >>output, GENERATED_WARNING
-
-    heading = 'Grok %s packages' % release
-    print >>output, heading
-    print >>output, '=' * len(heading)
-
-    included = packages(config, 'included')
-    package_list(included, config, output)
-
-    deprecating = packages(config, 'deprecating')
-    if deprecating:
-        print >>output, 'Deprecating'
-        print >>output, '-----------'
-        package_list(deprecating, versions, output)
-
-    print >>output, 'Dependencies'
-    print >>output, '------------'
-    all = config.options('versions')
-    dependencies = set(all) - (set(included) | set(deprecating))
-    package_list(dependencies, config, output, DEPENDENCY_PACKAGE_LINE)
-    output.close()
-
-print "Writing overview"
-
-output = open(os.path.join('doc', 'releases', 'index.rst'), 'w')
-print >>output, GENERATED_WARNING
-print >>output, """
+    print >>output, """
 Releases
 ========
 
@@ -128,21 +135,17 @@ included.
     :maxdepth: 1
 
 """
+    for release, location in releases:
+        print >>output, """
+    overview-%s""" % release
 
-for release, location in releases:
-    print >>output, """
-    overview-%s\
-""" % release
-
-
-for release, location in releases:
-    overview = open(
-        os.path.join('doc', 'releases', 'overview-%s.rst' % release), 'w')
-    print >>overview, GENERATED_WARNING
-    title = "Grok %s" % release
-    print >>overview, title
-    print >>overview, "=" * len(title)
-    print >>overview, """
+    for release, location in releases:
+        overview = open(os.path.join(path, 'overview-%s.rst' % release), 'w')
+        print >>overview, GENERATED_WARNING
+        title = "Grok %s" % release
+        print >>overview, title
+        print >>overview, "=" * len(title)
+        print >>overview, """
 This document covers major changes in this release that can lead to
 backward-incompatibilities and explains what to look out for when updating.
 
@@ -156,5 +159,11 @@ See the separate `package list <packages-%s.html>`_ document.
 
 """ % release
 
-    #overview.write((location/'README.txt').read())
-    overview.close()
+        #overview.write((location/'README.txt').read())
+        overview.close()
+
+if __name__ == '__main__':
+    releases = get_releases()
+    path = os.path.abspath(os.path.join('releases'))
+    write_package_lists(releases, path)
+    write_overview(releases, path)
